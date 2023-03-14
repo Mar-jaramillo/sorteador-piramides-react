e2661528a1ec230fab2ad75383d7e2970d8b779e
Valar789 <luismzo789@gmail.com>
2023-03-13 22:18:21 -0500

    update 2

--- src/utils/GeneratorsParticipants.js
+++ src/utils/GeneratorsParticipants.js
@@ -1,30 +1,32 @@
 export const firstRound = (pyramid, Group) => {
   const copyGroup = Group.slice(); // Creo una copia
   const raffleGroup = copyGroup.sort(() => Math.random() - 0.5); // lo rifo
-  const participantesPairs = new Array(pyramid); //creo arrray vacio con las posiciones
-  // Iterar sobre los objetos y emparejarlos creo
-  for (let i = 0; i < participantesPairs.length; i += 2) {
-    const obj1 = raffleGroup[i];
-    const obj2 = raffleGroup[i + 1] || "Bay"; // Si el array tiene un número impar de objetos, el último objeto se empareja con null
-    // Si las delegaciones son iguales, mover el segundo objeto a la siguiente posición y emparejarlo con el siguiente objeto
-    if (obj1["Delegación"] === obj2["Delegación"]) {
-      raffleGroup.splice(i + 1, 1);
-      raffleGroup.splice(i + 1, 0, obj2);
-      participantesPairs.push([obj1, raffleGroup[i + 2] || null]); // Emparejar el primer objeto con el siguiente objeto en la nueva posición
-    } else {
-      participantesPairs.push([obj1, obj2]); // Emparejar los objetos
+  const participantesPairs = new Array(pyramid).fill({}); //creo arrray vacio con las posiciones
+
+  let indexWhile = 0;
+  while (indexWhile < participantesPairs.length) {
+    const obj1 = raffleGroup[indexWhile]; //0,2
+    const obj2 = raffleGroup[indexWhile + 1]; //1,3
+    if (obj1["Delegación"] !== obj2["Delegación"]) {
+      participantesPairs[indexWhile] = obj1
+      participantesPairs[indexWhile + 1] = obj2
+      console.log(participantesPairs[indexWhile]);j
+      console.log(participantesPairs[indexWhile+1]);
     }
+    indexWhile += 2;
   }
+
+console.log(participantesPairs);
   const renderizadoParticipants = [];
-  for (let i = 0; i < copyGroup.length; i++) {
-    renderizadoParticipants.push({
-      id: i,
-      teams: [
-        { name: `${participantesPairs[i][0]["Nombre Deportista"]}` },
-        { name: `${participantesPairs[i][0]["Delegación"]}` },
-      ],
-    });
-  }
+  // for (let i = 0; i < copyGroup.length; i++) {
+  //   renderizadoParticipants.push({
+  //     id: i,
+  //     teams: [
+  //       { name: `${participantesPairs[i]["Nombre Deportista"]}` },
+  //       { name: `${participantesPairs[i]["Delegación"]}` },
+  //     ],
+  //   });
+  // }
   return renderizadoParticipants;
 };
 
