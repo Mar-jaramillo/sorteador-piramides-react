6155a6e2db475ea53caa37b8767997be3f56370e
Valar789 <luismzo789@gmail.com>
2023-03-16 15:47:13 -0500

    styled pyramid

--- src/components/templatesPyramids/BodyTemplate.jsx
+++ src/components/templatesPyramids/BodyTemplate.jsx
@@ -5,24 +5,21 @@ import TablePositions from "./TablePositions";
 
 export default function BodyTemplate({ typePyramid }) {
   return (
-    <div
+    <details
       id="capture"
-      className="grid place-content-center rounded-2xl bg-white p-7 mb-7"
+      className="grid place-content-center rounded-2xl  bg-white p-10 mb-7"
     >
-      <details className="">
-        <summary className="text-bluePrimary py-2 font-semibold bg-gray-300/50 w-96 rounded-lg px-4 text-lg hover:text-blueSecondary focus:outline-none focus:text-blue-500 transition duration-150 ease-in">
-         Presione aquí para expandir pirámide {typePyramid}
-        </summary>
-        <div className="py-2">
-          <Pyramid typePyramid={typePyramid} />
-        </div>
-        
-      </details>
-
+      <h3 className="text-gray-700 text-center text-xl font-bold my-6">CODIGO DSLDASLÑDKSD</h3>
+      <summary className="text-bluePrimary py-2 font-semibold bg-gray-300/50 max-w-md rounded-lg px-4 text-lg hover:text-blueSecondary focus:outline-none focus:text-blue-500 transition duration-150 ease-in">
+        Pirámide de {typePyramid} Competidores
+      </summary>
+      <div className="py-2">
+        <Pyramid typePyramid={typePyramid} />
+      </div>
       <div className="flex  justify-between px-12 mb-10 items-center w-full">
         <TablePositions />
         <TableCategories />
       </div>
-    </div>
+    </details>
   );
 }
--- src/views/Templates.jsx
+++ src/views/Templates.jsx
@@ -6,17 +6,17 @@ import BodyTemplate from "../components/templatesPyramids/BodyTemplate";
 import { handleCapture } from "../utils/handleCapture";
 
 export default function Templates() {
-  const [typePyramid, setTypePyramid] = useState(2);
+  const [typePyramid, setTypePyramid] = useState(3);
 
   return (
-    <div id="pyramid" className="p-10 text-white h-screen px-32">
+    <div id="pyramid" className="p-10 text-white min-h-screen h-full px-32">
       <BreadCrumb />
-      <h2 className="text-3xl mb-10 font-bold">
+      <h2 className="text-3xl font-bold  mb-16">
         Pirámide de {typePyramid} competidores ( Grupo XXXXXXX ){" "}
       </h2>
       {/* <HeaderTemplates typePyramid={typePyramid} setTypePyramid={setTypePyramid} /> */}
-      <div className="flex justify-between mb-4 ">
-        <p className="text-xl font-semibold">2 de 18 grupos sorteados</p>
+      <div className="flex justify-between  mb-6">
+        <p className="text-xl flex flex-col justify-end font-semibold">2 de 18 grupos sorteados</p>
         <div className="flex gap-5">
           <button  onClick={()=>handleCapture(typePyramid)}  className="btnPrimary w-32">Imprimir PDF</button>
           <button className="w-36 rounded-lg bg-green-500 ">
@@ -25,6 +25,10 @@ export default function Templates() {
         </div>
       </div>
       <BodyTemplate typePyramid={typePyramid} />
+      <div className="grid place-content-center">
+      <button className="w-40 border py-2 bg-white/25 rounded-lg"> &lt;&lt; Volver</button>
+      </div>
+    
     </div>
   );
 }
