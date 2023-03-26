dfa7feb7eefbdc107dd77ec553dbbd2125ceb40a
angimarcela <a.marcela.m.j@gmail.com>
2023-03-26 15:15:34 -0500

    Ajuste de función PDF

--- src/utils/handleCapture.js
+++ src/utils/handleCapture.js
@@ -1,6 +1,68 @@
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
+export const handleCapture = (typePyramid) => {
+  let contenido = document.getElementById("capture");
+
+  // Opcionalmente, descargar también una imagen PNG de la pirámide
+  html2canvas(contenido, { scale: 2 }).then((canvas) => {
+    let link = document.createElement("a");
+    link.download = `Piramide de ${typePyramid}.png`;
+    link.href = canvas.toDataURL();
+    link.click();
+  });
+
+  // Convertir la imagen en PDF y descargarla
+  setTimeout(() => {
+    html2canvas(contenido, { scale: 1 }).then((canvas) => {
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const pageSize = {
+        width: 215.9, // Ancho de una página carta en mm
+        height: 279.4 // Altura de una página carta en mm
+      };
+      const scale = Math.min(pageSize.width / imgWidth, pageSize.height / imgHeight);
+      const imgWidthScaled = imgWidth * scale;
+      const imgHeightScaled = imgHeight * scale;
+      const xPos = (pageSize.width - imgWidthScaled) / 2; // Calcular la posición horizontal para centrar la imagen
+      const yPos = (pageSize.height - imgHeightScaled) / 2; // Calcular la posición vertical para centrar la imagen
+      const pdf = new jsPDF("p", "mm", "letter");
+      pdf.addImage(
+        canvas.toDataURL("image/jpeg"),
+        "JPEG",
+        xPos,
+        yPos,
+        imgWidthScaled,
+        imgHeightScaled
+      );
+      pdf.save(`Piramide de ${typePyramid}.pdf`);
+    });
+  }, 1000);
+}  
+
+
+
+
+
+
+//   setTimeout(() => {
+//     html2canvas(contenido, { scale: 1 }).then((canvas) => {
+//       const imgWidth = canvas.width;
+//       const imgHeight = canvas.height;
+//       const pdf = new jsPDF("p", "mm", "a4");
+//       pdf.addImage(
+//         canvas.toDataURL("image/jpeg"),
+//         "JPEG",
+//         0,
+//         0,
+//         pdf.internal.pageSize.getWidth(),
+//         pdf.internal.pageSize.getHeight()
+//       );
+//       pdf.save(`Piramide de ${typePyramid}.pdf`);
+//     });
+//   }, 1000);
+// };
+
 // export const handleCapture = (typePyramid) => {
 //   let contenido = document.getElementById("capture");
 
@@ -24,36 +86,6 @@ import html2canvas from 'html2canvas';
 //     });
 //   }, 1000);
 // }
-
-export const handleCapture = (typePyramid) => {
-  let contenido = document.getElementById("capture");
-
-  // Opcionalmente, descargar también una imagen PNG de la pirámide
-  html2canvas(contenido, { scale: 2 }).then((canvas) => {
-    let link = document.createElement("a");
-    link.download = `Piramide de ${typePyramid}.png`;
-    link.href = canvas.toDataURL();
-    link.click();
-  });
-
-  // Convertir la imagen en PDF y descargarla
-  setTimeout(() => {
-    html2canvas(contenido, { scale: 1 }).then((canvas) => {
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-      const pdf = new jsPDF("p", "mm", "a4");
-      pdf.addImage(
-        canvas.toDataURL("image/jpeg"),
-        "JPEG",
-        0,
-        0,
-        pdf.internal.pageSize.getWidth(),
-        pdf.internal.pageSize.getHeight()
-      );
-      pdf.save(`Piramide de ${typePyramid}.pdf`);
-    });
-  }, 1000);
-};
 
 //Descargar ambos archivos falta arreglar documento pdf
 // import jsPDF from 'jspdf';
