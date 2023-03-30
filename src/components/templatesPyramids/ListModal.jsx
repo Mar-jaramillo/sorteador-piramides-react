df96e5ae2b4921f33a9ad4d0267e2ec1fd23c24c
Valar789 <luismzo789@gmail.com>
2023-03-30 14:19:27 -0500

    fix key a CODEP listModal

--- src/components/templatesPyramids/ListModal.jsx
+++ src/components/templatesPyramids/ListModal.jsx
@@ -5,7 +5,7 @@ const ListModal = ({ group }) => {
     <div className="border-white w-full text-sm overflow-auto">
       {group.length > 0 &&
         group.map((deportista, index) => (
-          <p key={deportista["CBTE IND"]} className={`fade-in${index} border-b p-1`}>
+          <p key={deportista["CodDep"]} className={`fade-in${index} border-b p-1`}>
             {deportista["Nombre Deportista"]}
           </p>
         ))}
