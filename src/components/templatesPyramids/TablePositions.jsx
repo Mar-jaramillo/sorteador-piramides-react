7fba3a03824ae605e1b74143cca5301b524359a5
Valar789 <luismzo789@gmail.com>
2023-04-11 11:23:57 -0500

    condicionar puestos

--- src/components/templatesPyramids/TablePositions.jsx
+++ src/components/templatesPyramids/TablePositions.jsx
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
+import GlobalContext from "../../utils/GlobalContext";
+import { getLocalStorage } from "../../utils/getLocalStorage";
 
 export default function TablePositions() {
+  const typePyramid = getLocalStorage("typePyramid");
+
   return (
     <div className="px-12">
-    <div className="flex justify-end p-1 text-gray-500">
+      <div className="flex justify-end p-1 text-gray-500">
         <p className="border py-1 px-14 rounded-l-lg bg-gray-200 border-gray-400">
-        Primer puesto     
+          Primer puesto
         </p>
-        <div className="border px-32 rounded-r-lg border-gray-400">
+        <div className="border px-32 rounded-r-lg border-gray-400"></div>
+      </div>
+      {typePyramid >= 2 && (
+        <div className="flex justify-end p-1 text-gray-500">
+          <p className="border py-1  px-12 rounded-l-lg bg-gray-200 border-gray-400">
+            Segundo puesto
+          </p>
+          <div className="border px-32 rounded-r-lg border-gray-400"></div>
         </div>
-    </div>
-    <div className="flex justify-end p-1 text-gray-500">
-        <p className="border py-1  px-12 rounded-l-lg bg-gray-200 border-gray-400">
-        Segundo puesto
-        </p>
-        <div className="border px-32 rounded-r-lg border-gray-400">
+      )}
+      {typePyramid >= 3 && (
+        <div className="flex justify-end p-1 text-gray-500 ">
+          <p className="border py-1  px-14 rounded-l-lg bg-gray-200 border-gray-400">
+            Tercer puesto
+          </p>
+          <div className="border px-32 rounded-r-lg border-gray-400"></div>
         </div>
-    </div>
-    <div className="flex justify-end p-1 text-gray-500 ">
-        <p className="border py-1  px-14 rounded-l-lg bg-gray-200 border-gray-400">
-        Tercer puesto
-        </p>
-        <div className="border px-32 rounded-r-lg border-gray-400">
+      )}
+
+      {typePyramid >= 8 && (
+        <div className="flex justify-end p-1 text-gray-500">
+          <p className="border py-1  px-14 rounded-l-lg bg-gray-200 border-gray-400">
+            Cuarto puesto
+          </p>
+          <div className="border px-32 rounded-r-lg border-gray-400"></div>
         </div>
-    </div>
-    <div className="flex justify-end p-1 text-gray-500">
-        <p className="border py-1  px-14 rounded-l-lg bg-gray-200 border-gray-400">
-        Cuarto puesto
-        </p>
-        <div className="border px-32 rounded-r-lg border-gray-400">
-        </div>
-    </div>
+      )}
     </div>
   );
 }
