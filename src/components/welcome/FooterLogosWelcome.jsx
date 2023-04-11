050f8cacf439fb22344845cdaad13a07473a41c3
Valar789 <luismzo789@gmail.com>
2023-04-11 10:51:28 -0500

    comentar codigo de la federacion

--- src/components/welcome/FooterLogosWelcome.jsx
+++ src/components/welcome/FooterLogosWelcome.jsx
@@ -4,17 +4,17 @@ import logoqubulowhite from "../../assets/logos/logoqubulowhite.png";
 
 export default function FooterLogosWelcome() {
   return (
-    <div className="flex  justify-center gap-16 w-full  ">
-      <div className="">
+    <div className="flex  justify-center text-center gap-16 w-full  ">
+      {/* <div className="">
         <p className="col-span-6 text-sm mb-3">Un producto de:</p>
         <a href="https://www.fedecolhap.com/">
           <img className="col-span-6 h-20" src={logofederacion} alt="" />
         </a>
-      </div>
+      </div> */}
       <div>
-        <p className="col-span-6 text-sm mb-8">Desarrollado por:</p>
+        <p className="col-span-6 text-sm ">Desarrollado por:</p>
         <a href="https://qubilo.com/">
-          <img className="col-span-6   h-10  " src={logoqubulowhite} alt="" />
+          <img className="col-span-6   h-12  " src={logoqubulowhite} alt="" />
         </a>
       </div>
     </div>
